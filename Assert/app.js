c6f0cb0704360c30fe5fa500cac23395f0279342
Naresh Shetty <nareshshetty.2011@gmail.com>
2016-08-09 19:50:33 +0530

    Add files via upload

---
+++ Assert/app.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+function add (a, b) {
+  return a + b;
+}
+
+var expected = add(1,2);
+
+assert( expected === 3, 'one plus two is three');
+
+assert( expected === 4, 'one plus two is three');
+
+assert.ok( expected === 3, 'one plus two is three');
+
+assert.equal(expected, 3, 'one plus two is three');
+
+assert(expected != 4, 'one plus two is three');
+
+assert.ok(expected != 4, 'one plus two is three');
+
+assert.notEqual(expected, 4, 'one plus two is three (NOT Four!)');
