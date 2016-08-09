edfc11d69bac3e7c2c4e21e5712f31c6030a2845
Naresh Shetty <nareshshetty.2011@gmail.com>
2016-08-09 19:21:06 +0530

    Add files via upload

---
+++ Node Debug  Exec Time/app.js
@@ -0,0 +1,37 @@
+/*console.time("first"); 
+
+setTimeout(function () 
+{
+console.timeEnd("first"); 
+}, 500);
+
+console.time("first"); 
+
+
+
+console.time('second');
+
+setTimeout(function () 
+{
+console.timeEnd('second'); 
+}, 1000);
+
+console.time('Third');
+
+setTimeout(function ()
+{
+    console.timeEnd('Third'); 
+}, 2000);
+
+
+console.log('hello');
+debugger;
+console.log('world');*/
+
+
+for (var index = 0; index < 10; index++)
+{
+    var message = 'Loop ' + index; 
+    debugger;
+console.log(message);
+}
